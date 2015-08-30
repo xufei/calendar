867012a671d2475b024cae7ed2ecadcb1686f934
Xu Fei <xu.fei@outlook.com>
2015-08-30 20:17:56 +0800

    simple

---
+++ calendar.js
@@ -0,0 +1,40 @@
+'use strict';
+
+export default class Calendar {
+	constructor(year, month) {
+		this.year = year;
+		this.month = month;
+	}
+
+	static months = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
+	static weekdays = ["日", "一", "二", "三", "四", "五", "六"];
+	
+	set year(val) {
+		this._year = val;
+		
+		this.years = [];
+		let startIndex = Math.floor(val / 10) * 10 + 1;
+
+		for (let year = 0; year < 10; year++) {
+			this.years.push(startIndex + year);
+		}
+	}
+	
+	set month(val) {
+		this._month = val;
+		
+		this.calendar = [
+			[]
+		];
+
+		let offset = new Date(new Date(this._year, val, 1)).getDay();
+		let lastDay = new Date(new Date(this._year, val + 1, 1) - 1);
+
+		for (let day = offset; day < lastDay.getDate() + offset; day++) {
+			if (this.calendar[Math.floor(day / 7)]) {
+				this.calendar[Math.floor(day / 7)] = [];
+			}
+			this.calendar[Math.floor(day / 7)][day % 7] = day - offset + 1;
+		}
+	}
+}
